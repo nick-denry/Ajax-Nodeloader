76de17730e41f25417915d062999c1e8abbe57d2
root <root@all-for-wedding.ru>
2012-02-18 16:16:20 +0400

    Fix up comments and trailing spaces

--- js/ajax_nodeloader.js
+++ js/ajax_nodeloader.js
@@ -1,15 +1,15 @@
 (function ($) {
 	// Original JavaScript code.
-	$(document).ready(function(){  
-		
-		$('body').append('<div id="nodeloader-ajax-image">&nbsp;</div>');	  	  
-		
+	$(document).ready(function(){
+
+		$('body').append('<div id="nodeloader-ajax-image">&nbsp;</div>');
+
 		function nodeloader_click(){
 			$('#nodeloader-ajax-image').css('display','block');
 
 			// Unfortunally, we need to everride default explorer 7.0 behaviour
-			// see: 
-			// @link: http://stackoverflow.com/questions/7793728/get-a-relative-path-with-jquery-attr-property-with-ie7		
+			// see:
+			// @link: http://stackoverflow.com/questions/7793728/get-a-relative-path-with-jquery-attr-property-with-ie7
 			if (($.browser.msie) && ($.browser.version == '7.0'))
 			{
 				var link_href = $(this).attr('href').replace('http://'+window.location.hostname,'').substr(1).replace('node/','');
@@ -18,50 +18,43 @@
 			{
 				var link_href = $(this).attr('href').substr(1).replace('node/','');
 			}
-			
-			//console.log(link_href);
-			
 
-			//console.log();
+
+			//Make ajax call to module
 			$.ajax({
 				type: 'GET',
-				url: 'node_load/node/'+link_href,				
-				success: function(data){					
-					//console.log(data);
-					var node = eval('(' + data + ')');
-					//console.log(node);
-					$('#page-title').html(node.title);
-					$('.field-item').html(node.body);
-					$('#nodeloader-ajax-image').css('display','none');
+				url: 'node_load/node/'+link_href,
+				success: function(data){
+						//Process json answer
+						var node = eval('(' + data + ')');
 
+						//Set up values
+						$('#page-title').html(node.title);
+						$('.field-item').html(node.body);
+						$('#nodeloader-ajax-image').css('display','none');
 
-					// Set up links for tabs Display and Edit
-					if ($('ul.tabs.primary').length > 0) {
-					$('ul.tabs.primary > li > a').each(function(){
-						var new_href = $(this).attr('href').replace(/\d/g,node.nid);
-						$(this).attr('href',new_href);
-					});
+						// Set up drupal links for tabs Display and Edit
+						if ($('ul.tabs.primary').length > 0) {
+						$('ul.tabs.primary > li > a').each(function(){
+							var new_href = $(this).attr('href').replace(/\d/g,node.nid);
+							$(this).attr('href',new_href);
+						});
 					}
 
-					// Bind new links click event
-					//console.log($('a.nodeloader').length);
-
-
-
-					if ($('a.nodeloader').length > 0) {							
+					// Bind dynamically adding links click event
+					if ($('a.nodeloader').length > 0) {
 						$(document).undelegate('a.nodeloader','click').delegate('a.nodeloader','click',nodeloader_click);
 					}
-
-				//console.log($('ul.tabs.primary > li > a').attr('href'));			
 				},
 			dataType: 'ajax'
 			});
 
-		return false;
+			// We don't really want default click
+			return false;
 		}
-		
+
+		//Should replace this by someway
 	    $(document).delegate('ul#main-menu-links a','click',nodeloader_click);
-		
-		//$('').click(nodeloader_click());
+
 	});
 })(jQuery);
