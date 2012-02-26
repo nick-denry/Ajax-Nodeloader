675a0712c70e0e8040fc92360ee9485f14d57516
Nick Denry <nick-denry@1822902.no-reply.drupal.org>
2012-02-27 01:35:50 +0400

    Correct brackets to follow drupal standards
    Signed-off-by: Nick Denry <nick-denry@1822902.no-reply.drupal.org>

--- js/ajax_nodeloader.js
+++ js/ajax_nodeloader.js
@@ -1,20 +1,18 @@
 (function ($) {
     // Original JavaScript code.
-    $(document).ready(function(){
+    $(document).ready(function() {
 
         //Append nodeloader ajax image
         $('body').append('<div id="nodeloader-ajax-image">&nbsp;</div>');
 
         //try to load hashtag page
-        if (window.location.hash != '')
-        {
+        if (window.location.hash != '') {
             var full_link = window.location.hash.substr(1);
             nodeloader_load(full_link,$('a[href="'+full_link+'"]').attr('rel'));
         }
 
         //function to load page
-        function nodeloader_load(full_link,link_attr)
-        {
+        function nodeloader_load(full_link,link_attr) {
             //Display loader image
             $('#nodeloader-ajax-image').css('display','block');
             //Get clean "some.html" or node number
@@ -23,7 +21,7 @@
             $.ajax({
                 type: 'GET',
                 url: '/node_load/node/'+link_href,
-                success: function(data){
+                success: function(data) {
                         //Process json answer
                         //eval('(' + data + ')');
                         var node = jQuery.parseJSON(data);
@@ -66,10 +64,10 @@
 
                         // Set up drupal links for tabs Display and Edit
                         if ($('ul.tabs.primary').length > 0) {
-                        $('ul.tabs.primary > li > a').each(function(){
+                          $('ul.tabs.primary > li > a').each(function() {
                             var new_href = $(this).attr('href').replace(/\d/g,node.nid);
                             $(this).attr('href',new_href);
-                        });
+                          });
                         }
 
                         //Change hash for user
@@ -90,7 +88,7 @@
         }
 
 
-        function nodeloader_click(){
+        function nodeloader_click() {
 
             // Store current link for ajax call while detecting home
             var current_link = $(this);
@@ -98,12 +96,10 @@
             // Unfortunally, we need to override default explorer 7.0 behaviour
             // see:
             // @link: http://stackoverflow.com/questions/7793728/get-a-relative-path-with-jquery-attr-property-with-ie7
-            if (($.browser.msie) && ($.browser.version == '7.0'))
-            {
+            if (($.browser.msie) && ($.browser.version == '7.0')) {
                 var full_link = $(this).attr('href').replace('http://'+window.location.hostname,'');
             }
-            else
-            {
+            else {
                 var full_link = $(this).attr('href');
             }
 
