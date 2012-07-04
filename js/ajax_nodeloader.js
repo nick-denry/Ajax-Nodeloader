26730b3ff0b09be4fd0afdc8aa2c3c675d7b3e56
Nick Denry <nick-denry@1822902.no-reply.drupal.org>
2012-07-04 15:58:32 +0400

    Set proper link to load via back and forward browser buttons.
    Set previous hashtag to current window.location.hash to avoid multiple loads
    via setInterval.
    
    Signed-off-by: Nick Denry <nick-denry@1822902.no-reply.drupal.org>

--- js/ajax_nodeloader.js
+++ js/ajax_nodeloader.js
@@ -12,7 +12,14 @@
     // TODO: Replace setInterval with setTimeot with callback of nodeloader_load.
     setInterval(function() {
       if (typeof(Drupal.settings.ajax_nodeloader.prev_hash) !== 'undefined' && window.location.hash !== Drupal.settings.ajax_nodeloader.prev_hash) {
-        var full_link = window.location.hash == '' ? Drupal.settings.ajax_nodeloader.front_page : window.location.hash.substr(1);
+        if (window.location.hash == '') {
+          var full_link = window.location.pathname == '/'?Drupal.settings.ajax_nodeloader.front_page:window.location.pathname;
+        }
+        else {
+          var full_link = window.location.hash.substr(1);
+        }
+        Drupal.settings.ajax_nodeloader.prev_hash = window.location.hash;
+
         nodeloader_load(full_link, $('a[href="'+full_link+'"]').attr('rel'));
       }
     },1000);
