0b6042b0583a902f11822daa5b82d08986db5f3a
Nick Denry <nick-denry@1822902.no-reply.drupal.org>
2012-07-04 14:18:49 +0400

    Add front page node prefix.
    Signed-off-by: Nick Denry <nick-denry@1822902.no-reply.drupal.org>

--- js/ajax_nodeloader.js
+++ js/ajax_nodeloader.js
@@ -10,7 +10,7 @@
     // Added support for loading site front page node.
     // TODO: Also add HTML 5 history support there.
     setInterval(function() {
-      if (typeof(Drupal.settings.ajax_nodeloader.prev_hash) == 'undefined' && window.location.hash !== Drupal.settings.ajax_nodeloader.prev_hash) {
+      if (typeof(Drupal.settings.ajax_nodeloader.prev_hash) !== 'undefined' && window.location.hash !== Drupal.settings.ajax_nodeloader.prev_hash) {
         var full_link = window.location.hash == '' ? Drupal.settings.ajax_nodeloader.front_page : window.location.hash.substr(1);
         nodeloader_load(full_link, $('a[href="'+full_link+'"]').attr('rel'));
       }
